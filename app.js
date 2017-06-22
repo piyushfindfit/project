546b6e540a4c514c46fd798e7c8e77e61d401ad1
Piyaush Kumar Rana <piyaush.rana@BRILLIO.COM>
2017-06-22 20:48:00 +0530

    new app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
@@ -22,6 +22,7 @@ app.get('/contact',function(req,res){
   res.sendFile(path.join(__dirname+'/contact.html'));
 });
 
-app.listen(process.env.PORT || 3000);
 
-console.log("Running at Port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
